9f76af49e40a262767c1566ce5e066143dc30ed2
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-09-11 16:11:06 +0200

    third commit

--- index_1.js
+++ index_1.js
@@ -4,11 +4,25 @@ var config = require('./config');
 
 var T = new Twit(config);
 
+setInterval(tweetIt, 1000*20);
+
 tweetIt();
 
+
 function tweetIt(){
+    //to have different tweets always as we get error when tweet gets repeated
+    /*
+    var r = Math.floor(Math.random()*100);
     var tweet = {
-        status: '#HappyHalloween!!!'
+        status: 'Here is random number' + r  + '#HappyHalloween!!!'
+    }
+    */ 
+   var r = Math.floor(Math.random()*100);
+
+
+    var tweet = {
+        status: 'Here is random number' + r  + '#HappyHalloween!!!'
+        //status: '#HappyHalloween!!!'
     }
     
     T.post('statuses/update', tweet, tweeted);

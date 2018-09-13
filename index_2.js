7a2064078b749868011aa7c2e08569e6db45f4d7
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-09-13 11:48:26 +0200

    eighth commit

--- index_2.js
+++ index_2.js
@@ -6,6 +6,12 @@ var config = require('./config');
 var T = new Twit(config);
 
 
+
+var exec = require('child_process').exec;
+var fs = require('fs');
+
+
+
 //Setting up a user stream
 var stream = T.stream('user');
 
@@ -25,7 +31,7 @@ setInterval(tweetIt, 1000*20);
 //tweetIt();
 
 
-function tweetIt(txt){
+function tweetIt(){
     //to have different tweets always as we get error when tweet gets repeated
     /*
     var r = Math.floor(Math.random()*100);
@@ -33,6 +39,28 @@ function tweetIt(txt){
         status: 'Here is random number' + r  + '#HappyHalloween!!!'
     }
     */ 
+    
+    var cmd = 'processing-java --sketch="%../sketch%" --run';
+
+    exec(cmd, processing);
+    function processing(){
+        var filename = 'sketch/output.png';
+        var params = {
+            encoding: 'base64'
+        }
+        var b64content = fs.readFileSync(filename, params);
+        T.post('media/upload', { media_data: b64content }, uploaded );
+
+        function uploaded(err, data, response){
+            var id = data.media_id_string
+            var tweet = {
+                status: '#codingnode from node.js'
+            }
+        }
+    }
+
+    
+
     var tweet = {
         status: txt
     }

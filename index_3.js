8967797fd8dd369a0f1feb8c4396de449faaf95a
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-09-14 13:24:17 +0200

    tenth commit

---
+++ index_3.js
@@ -0,0 +1,34 @@
+console.log('The replier bot is starting');
+var Twit = require('twit');
+var config = require('./config');
+var T = new Twit(config);
+
+//Setting up a user stream
+var stream = T.stream('user');
+
+//Anytime someone follows me 
+stream.on('tweet', tweetEvent);
+//a callback for 'tweet' event, when someone tweets me The function is executed
+function tweetEvent(eventMsg){
+    var fs = require('fs');
+    var json = JSON.stringify(eventMsg,null,2);
+    fs.writeFile("tweet.json", json);
+}
+function tweetIt(txt){
+    //to have different tweets always as we get error when tweet gets repeated
+    //var r = Math.floor(Math.random()*100);
+    var tweet = {
+        status: txt
+    }
+    T.post('statuses/update', tweet, tweeted);
+    
+    function tweeted(err, data, response) {
+        if(err){
+            console.log('Something went wrong');
+        }
+        else{
+            console.log('It worked!!');
+        }
+        console.log(data)
+      };
+}

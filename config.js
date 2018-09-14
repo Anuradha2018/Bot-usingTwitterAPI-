9e8eb4b431a26c3b735cefe9bd1de6ae56787f25
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-09-14 13:01:39 +0200

    nineth commit

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key:         'u1CdjwjEahIppPHfZbyIUEaKb',
-    consumer_secret:      'xRRMby5FYDyuQJlysYjqPQTwi8YjTSq4nVVuTwMyoNQjrBf2IT',
+    consumer_key:         '9j2vXEqXI9fR6VQxEhZTbj1hW',
+    consumer_secret:      'gdt1D2bcYRzpKbWCqOGQCQixGjb5N4wDBeaVlUvFknOnBpQH4y',
     access_token:         '1039085116344225794-60Rm60Wj5VoA5KrTXCRL80NFnLbrpS',
     access_token_secret:  '1QoO8d1gCKrUYVfeDyj40yRGf2rMFwtNGsDePfxX4seUk'
 }
--- index_2.js
+++ index_2.js
@@ -1,71 +1,32 @@
 console.log('The follow bot is starting');
 var Twit = require('twit');
-
 var config = require('./config');
-
 var T = new Twit(config);
-
-
-
-var exec = require('child_process').exec;
-var fs = require('fs');
-
-
 
 //Setting up a user stream
 var stream = T.stream('user');
 
 //Anytimesomeone follows me 
+
 stream.on('follow', followed);
-
-
 //a callback for 'follow' event, when someone follows me The function is executed
 function followed(eventMsg){
+    console.log('follow event');
     var name = eventMsg.source.name;
     var screenName = eventMsg.source.screen_name;
-    tweetIt('@' + screenName + ' Thank You for following me');
+    tweetIt('@'  + screenName  +' Thank You for following me');
 }
 
-setInterval(tweetIt, 1000*20);
+//setInterval(tweetIt, 1000*20);
 
 //tweetIt();
-
-
-function tweetIt(){
+function tweetIt(txt){
     //to have different tweets always as we get error when tweet gets repeated
-    /*
-    var r = Math.floor(Math.random()*100);
-    var tweet = {
-        status: 'Here is random number' + r  + '#HappyHalloween!!!'
-    }
-    */ 
-    
-    var cmd = 'processing-java --sketch="%../sketch%" --run';
-
-    exec(cmd, processing);
-    function processing(){
-        var filename = 'sketch/output.png';
-        var params = {
-            encoding: 'base64'
-        }
-        var b64content = fs.readFileSync(filename, params);
-        T.post('media/upload', { media_data: b64content }, uploaded );
-
-        function uploaded(err, data, response){
-            var id = data.media_id_string
-            var tweet = {
-                status: '#codingnode from node.js'
-            }
-        }
-    }
-
-    
-
+    //var r = Math.floor(Math.random()*100);
     var tweet = {
         status: txt
     }
-    
-    T.post('statuses/update', tweet, tweeted);
+   T.post('statuses/update', tweet, tweeted);
     
     function tweeted(err, data, response) {
         if(err){
@@ -77,3 +38,4 @@ function tweetIt(){
         console.log(data)
       };
 }
+
